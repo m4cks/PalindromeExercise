9464d6c16400c51bd90ed264f0ffe28f99a99b0c
Max Schlapfer <max.schlapfer@gmail.com>
2022-10-11 00:55:10 -0500

    Palindrome Tester is functional!

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+            string baseCheck = "";
+            string check = "";
+            for(int i = 0; i < str.Length; i++)
+            {
+                if(Char.IsLetter(str[i]) || Char.IsDigit(str[i]))
+                    baseCheck += str[i];
+                if(Char.IsLetter(str[(str.Length - 1 - i)]) || Char.IsDigit(str[(str.Length - 1 - i)]))
+                    check += str[(str.Length - 1 - i)].ToString();
+            }
+            if(baseCheck == check)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("race car", true)]
+        [InlineData("forking", false)]
+        [InlineData("poop", true)]
+        [InlineData("killa", false)]
+        public void Test1(string word, bool expected)
         {
-
+            //Arrange
+            WordSmith smith = new WordSmith();
+            //Add
+            bool actual = smith.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
+
     }
 }
